9446790c858017e4bbdf9bd8807af2b25155c2cb
Ranjith S <138761903+ranjiths-17904@users.noreply.github.com>
2024-09-12 12:25:30 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,19 @@
+let x = "Ranjith";
+let y = 118;
+console.log(x + " " + y);
+
+let dat = 
+{
+  name : "Ranjith S",
+  Roll : "22CS118",
+  tel : 7395860211
+}
+
+let data = {
+  "name" : "Ranjith S",
+  "Roll" : "22CS118",
+  "tel" : 7395860211
+}
+
+console.log(dat);
+console.log(data);
